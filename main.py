0dafc002a9b62bda3129e95c53aee8f9acda5b16
ItzSkyRed <93844414+ItzSkyReed@users.noreply.github.com>
2023-07-27 22:52:09 +0500

    python_issue

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ from pytube import Playlist, YouTube
 
 def get_urls(playlist_url):
     playlist = Playlist(playlist_url)
-    playlist_len = len(playlist)
-    for i in range(playlist_len):
-        video = YouTube(playlist[i])
-        download = video.streams.filter(only_audio=True)
-        download[0].download('audios')
-        print(f'Audios left {playlist_len - i}')
+    for i in range(len(playlist)):
+        video = YouTube(playlist[i]).streams.filter(only_audio=True)
+        video[0].download('audios')
+        print(f'Audios left {len(playlist) - i}')
+
 
 get_urls(input('Link to youtube playlist\n'))
